502a8db42a70ecc79b4e609e4f23491b3b55e285
佐藤光 <hs@hicalu-MBP2020.local>
2020-11-09 11:36:32 +0900

    abc182a

---
+++ abc182a.cpp
@@ -0,0 +1,14 @@
+//abc181a
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    
+    int a,b;
+    cin >> a >> b;
+    int ans = 2 * a + 100 - b;
+
+    cout << ans << endl;
+    return 0;
+    }
