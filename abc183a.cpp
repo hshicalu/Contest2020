ae73c4d7f20ff6421084525663333b6b31262ae9
佐藤光 <hs@hicalu-MBP2020.local>
2020-11-21 17:16:50 +0900

    abc183a

---
+++ abc183a.cpp
@@ -0,0 +1,21 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int x;
+    cin >> x;
+
+    if (x >= 0) {
+        x = x;
+        }
+    else {
+        x = 0;
+        }
+
+    cout << x << endl;
+    return 0;
+    }
+
+
