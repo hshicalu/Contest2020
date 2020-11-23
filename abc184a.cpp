b401618962d98a36914ad7f9bffccdb4a4e0833e
佐藤光 <hs@hicalu-MBP2020.local>
2020-11-23 17:43:00 +0900

    abc184a

---
+++ abc184a.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int a,b,c,d;
+    cin >> a >> b;
+    cin >> c >> d;
+    cout << a * d - b * c << endl;
+    return 0;     
+
+    }
+
+
