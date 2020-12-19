8eddaaf787ecc6b58dc3e7811177b04087c6cf1f
佐藤光 <hs@hicalu-MBP2020.local>
2020-12-19 09:28:38 +0900

    abc185c

---
+++ abc185c.py
@@ -0,0 +1,13 @@
+from operator import mul
+from functools import reduce
+
+l = int(input())
+def cmb(n,r):
+    r = min(n-r,r)
+    if r == 0: return 1
+    over = reduce(mul, range(n, n - r, -1))
+    under = reduce(mul, range(1,r + 1))
+    return over // under
+
+ans = cmb(l-1, 11)
+print(ans)
